45ff86de68f6a6adff44140ecdd3520e88fe8673
datfc97pro <datfc97pro@gmail.com>
2019-03-14 10:56:07 +0700

    update

--- env-config.js
+++ env-config.js
@@ -2,8 +2,8 @@ const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
   "process.env.BASE_URL": prod
-    ? "http://localhost:3000"
+    ? "https://datngo-portfolios.herokuapp.com"
     : "http://localhost:3000",
-  "process.env.NAMESPACE": "http://localhost:3000",
+  "process.env.NAMESPACE": "https://datngo-portfolios.herokuapp.com",
   "process.env.CLIENT_ID": "CtN5838LtCFYpAfjTCL3F3aw98zc0PGF"
 };
